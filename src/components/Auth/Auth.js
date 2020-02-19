6d24e9054eb8a338562cc95f490b052024818349
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2020-02-19 21:52:53 +0200

    update Auth.js

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { Route, Redirect } from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 
-const AuthRoute = ({ children, log, ...rest }) => {
-  // const log = false;
-  return (
-    <Route
-      {...rest}
-      render={() =>
-        log ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-            }}
-          />
-        )
-      }
-    />
-  );
-};
+const AuthRoute = ({children, log, ...rest}) => (
+  <Route
+    {...rest}
+    render={() =>
+      log ? (
+        children
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+          }}
+        />
+      )
+    }
+  />
+);
+
 
 export default AuthRoute;
